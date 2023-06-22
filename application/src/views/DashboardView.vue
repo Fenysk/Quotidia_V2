0bad06e6d15fd5bebf1396bb6639d8557d9397f1
Fenysk <minefenysk@gmail.com>
2023-06-22 09:11:15 +0200

    :package: Ajout du préprocesseur SCSS

--- application/src/views/DashboardView.vue
+++ application/src/views/DashboardView.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
+    <div id="dashboard" class="
+            w-screen
+            h-screen
+            bg-gray-300
+    ">
+
         <h1>Dashboard</h1>
         <button @click="disconnect">Se déconnecter</button>
+
     </div>
 </template>
+
+
 
 <script setup>
 import router from '../router/router.js'
@@ -12,5 +20,21 @@ const disconnect = () => {
     console.log('Try to disconnect...')
     localStorage.removeItem('token')
     router.push('/login')
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+
+</style>
+
+
+
+<script>
+tailwind.config = {
+    corePlugins: {
+        preflight: true
+    }
 }
 </script>
