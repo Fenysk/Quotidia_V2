241e1d245e840e194870652b63b089a5490de745
Fenysk <minefenysk@gmail.com>
2023-06-29 10:56:37 +0200

    Update config.js

--- application/src/config/config.js
+++ application/src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   mode: 'production',
   API_URL_DEV: 'http://192.168.0.46:3000',
-  API_URL_PROD: 'https://quotidiav2-production.up.railway.app/',
+  API_URL_PROD: 'https://quotidiav2-production.up.railway.app',
 };
 
 export default config;
